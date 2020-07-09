dcbcb9f469385d58e4c5c0d877da915c22da3ac6
Nguyen Van Huy <huynguyen98.clc@gmail.com>
2020-07-09 19:45:36 +0700

    - update favorites movies function.

---
+++ MovieInformation/MovieInformation/Migrations/20200709084059_update favorite userid.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieInformation.Migrations
+{
+    public partial class updatefavoriteuserid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userName",
+                table: "MovieFavorites");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userId",
+                table: "MovieFavorites",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "MovieFavorites");
+
+            migrationBuilder.AddColumn<string>(
+                name: "userName",
+                table: "MovieFavorites",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
